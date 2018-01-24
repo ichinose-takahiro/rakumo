6e787f30a631f0496d4c38de49c6d34b29fcbf77
ichinose-takahiro <ichinose-takahiro@919.jp>
2018-01-24 09:38:46 +0900

    googleapi処理分の初期追加

---
+++ rakumo/libs/calendarResourceUpdate.py
@@ -0,0 +1,104 @@
+# 施設データを取ってくるやつや
+import os
+import httplib2
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+import csv
+import json
+
+WORKDIR = '/var/www/html/googleapi'
+RESOURCE = WORKDIR + '/data/updateResource_1221.csv'
+
+"会議室データを取得"
+def getResource():
+    resourceData = csvToJson(RESOURCE)
+    return resourceData
+"CSVを読み込みJSON化する"
+def csvToJson(csvData):
+    jsonData = []
+    with open(csvData, 'r',encoding='utf-8', errors='ignore', newline='') as f:
+        for line in csv.DictReader(f):
+            line_json = json.dumps(line, ensure_ascii=False)
+            jsonData.append(line_json)
+    return jsonData
+
+def getResourceData(service):
+    print('Getting the first 10 users in the domain')
+
+    upDateList = getResource()
+
+
+
+    for upDataData in upDateList:
+        upDataData = json.loads(upDataData,encoding='UTF-8')
+        print(upDataData['resourceId'])
+        print(upDataData['resourceName'])
+        print(upDataData['generatedResourceName'])
+        EVENT = {"resourceId":upDataData['resourceId'], "resourceName":upDataData['resourceName'], "generatedResourceName": upDataData['generatedResourceName'], "resourceType":upDataData['resourceType']}
+#        EVENT['resourceId'] = upDataData['resourceId']
+#        EVENT['resourceName'] = upDataData['resourceName']
+#        EVENT['generatedResourceName'] = upDataData['generatedResourceName']
+        print(EVENT)
+        resourcedatas = service.resources().calendars().update(customer='my_customer', calendarResourceId='appsadmin@919.jp', body=EVENT).execute()
+        print(resourcedatas)
+        print(upDataData['resourceId'] + ':update!')
+
+def main():
+
+    try:
+        import argparse
+
+        parser = argparse.ArgumentParser(parents=[tools.argparser])
+        flags = parser.parse_args()
+    except ImportError:
+        flags = None
+
+    # 認証情報を格納するディレクトリ「.credentials」の設定。ディレクトリが無い場合は作成
+    credential_dir = os.path.join(os.path.expanduser('~'), '.test_credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+
+    # 認証ファイルのパスを設定と読み込み
+    credential_path = os.path.join(credential_dir, 'admin-directory_v1_test.json')
+    store = Storage(credential_path)
+    credentials = store.get()
+
+    # 認証ファイルが無い場合は作成
+    if not credentials or credentials.invalid:
+
+        # 使用する機能の範囲を設定
+        scopes = [
+            'https://www.googleapis.com/auth/admin.directory.resource.calendar',
+        ]
+
+        # 認証キーの設定
+        secret_dir = os.path.join(os.path.expanduser('~'), '.test_secrets')
+        if not os.path.exists(secret_dir):
+            os.makedirs(secret_dir)
+
+        # 認証キーから認証処理を行うクラスのインスタンスを生成
+        # flow = client.flow_from_clientsecrets(
+        #     os.path.join(secret_dir, 'client_secret_test.json'), scopes)
+
+        flow = client.flow_from_clientsecrets( '../json/client_secret.json', scopes)
+
+        # アプリケーションの名前
+        flow.user_agent = 'User register Test Tool'
+
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else:  # Python 2.6　互換用処理
+            credentials = tools.run(flow, store)
+        print('証明書を保存しました： ' + credential_path)
+
+    # 認証を行う
+    http = credentials.authorize(httplib2.Http())
+    app_admin_service = discovery.build('admin', 'directory_v1', http=http)
+
+    getResourceData(app_admin_service)
+    print('complete')
+
+if __name__ == '__main__':
+    main()
