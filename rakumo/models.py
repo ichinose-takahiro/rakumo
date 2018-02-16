a15d46180725b93a05e3f91ad39278df0e103c42
ichinose-takahiro <ichinose-takahiro@919.jp>
2018-02-16 10:52:15 +0900

    Revert "ログイン画面のため追加"
    This reverts commit e58a8a6d68d05f8d396d43a08d79939bf5ef8855.

--- rakumo/models.py
+++ rakumo/models.py
@@ -3,19 +3,7 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from datetime import datetime
-from django.contrib import admin
-from django.contrib.auth.models import User
-from django.db import models
-
-from oauth2client.contrib.django_util.models import CredentialsField
 
 class FileNameModel(models.Model):
     file_name = models.CharField(max_length = 50)
     upload_time = models.DateTimeField(default = datetime.now)
-
-class CredentialsModel(models.Model):
-    id = models.ForeignKey(User, primary_key=True)
-    credential = CredentialsField()
-
-class CredentialsAdmin(admin.ModelAdmin):
-    pass
